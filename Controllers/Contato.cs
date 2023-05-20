02f0edbda1eb8d174325513f7f5cbdbe0fc79b0e
Leonardo Henrique <99227509+Sazakii@users.noreply.github.com>
2023-05-19 21:46:26 -0300

    segundo commit

---
+++ Controllers/Contato.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace apparteyment.Controllers
+{
+    public class Contato : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar()
+        {
+            return View();
+        }
+        public IActionResult Editar()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ApagarConfirmacao()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// evento que adiciona a classe active aos container que possuem a classe revela, que junto com o css, faz aparecer o conteudo ao scrollar.
+window.addEventListener('scroll', revelar);
+
+function revelar() {
+    let revelas = document.querySelectorAll('.revela');
+
+    for(let i = 0; i < revelas.length; i++) {
+
+        let tamanhoJanela = window.innerHeight;
+        let revelarTop = revelas[i].getBoundingClientRect().top;
+        let revelarPonto = 150;
+
+        if(revelarTop < tamanhoJanela - revelarPonto) {
+            revelas[i].classList.add('active');
+         } //else {
+        //     revelas[i].classList.remove('active');
+        // }
+    }
+}
+
