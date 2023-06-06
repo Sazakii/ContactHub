ae6d80ebb30d17d0d736c4a7ed756ccf95828b41
Leonardo Henrique <99227509+Sazakii@users.noreply.github.com>
2023-06-06 20:18:45 -0300

    alteração

--- apparteyment/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace apparteyment.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
